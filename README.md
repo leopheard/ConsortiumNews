93bd0c235257118eccb2466a23d26bbfadabf461
leopheard <leopheard.misc@gmail.com>
2019-09-06 22:53:27 -0400

    upload

---
+++ README.md
@@ -0,0 +1,10 @@
+<a href="https://consortiumnews.com/">Consortium News</a> podcasts.<br>
+
+<a href="kodi.tv">KODI<a> / XBMC / Firestick addon for the <a href="https://consortiumnews.com/">Consortium News</a> podcast.<br>
+
+Consortium News is the podcast for <a href="https://consortiumnews.com/">Consortiumnews.com</a>, a home for important, well-reported stories that challenge the dominant mainstream news media of our day. We focus on international and domestic policies, as well as political and media developments in the United States and abroad.  Consortium News was founded in 1995 as the first investigative news magazine on the internet by Robert Parry, one of the reporters who helped expose the Iran-Contra scandal for The Associated Press in the mid-1980s. Funded through small donations from readers, Consortium News is not beholden to any government or corporate special interests<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<img src="https://ssl-static.libsyn.com/p/assets/d/c/1/0/dc10ba58acef6bb3/Consortium_News_v4_1400.jpg">
+<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ resources/lib/mainaddon.py
@@ -0,0 +1,69 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url1 = "https://consortiumnews.libsyn.com/rss"
+def get_soup1(url1):
+    page = requests.get(url1)
+    soup1 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup1))
+    return soup1
+get_soup1("https://consortiumnews.libsyn.com/rss")
+
+def get_playable_podcast1(soup1):
+    subjects = []
+    for content in soup1.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': "https://ssl-static.libsyn.com/p/assets/d/c/1/0/dc10ba58acef6bb3/Consortium_News_v4_1400.jpg",
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast1(playable_podcast1):
+    items = []
+    for podcast in playable_podcast1:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast(soup1):
+    subjects = []
+    for content in soup1.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': "https://ssl-static.libsyn.com/p/assets/d/c/1/0/dc10ba58acef6bb3/Consortium_News_v4_1400.jpg",
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast(playable_podcast):
+    items = []
+    for podcast in playable_podcast:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
